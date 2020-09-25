52776c1ac434451c7262dc392054b15aef3f1042
Andrei Bostan <abostan.ir@gmail.com>
2020-09-25 12:56:30 +0200

    added profcile and sending files

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(express.static("public"));
 
 const port = process.env.PORT || 5002;
 
@@ -10,11 +11,11 @@ app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
-
 app.post("/cookies", (req, res) => {
   console.log("Got body:", req.body);
   res.send("Got the cookies :-)");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
 });
