28b932bd3610a61c3c534ddfda974e4c5b43e365
Andrei Bostan <abostan.ir@gmail.com>
2020-09-30 13:56:22 +0200

    made env variables

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const firebase = require("firebase-admin");
 const serviceAccount = require("./cookies-database-firebase-adminsdk-tmeah-a6eb68f4a9.json");
@@ -5,7 +6,18 @@ const app = express();
 const admin = require("firebase-admin");
 
 firebase.initializeApp({
-  credential: firebase.credential.cert(serviceAccount),
+  credential: firebase.credential.cert({
+    type: "service_account",
+    project_id: "cookies-database",
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri: "https://oauth2.googleapis.com/token",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  }),
   databaseURL: "https://cookies-database.firebaseio.com/",
 });
 
